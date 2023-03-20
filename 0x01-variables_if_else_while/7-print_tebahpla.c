d2e40361d0784c7bde416e55cdb15ea6a34dd30d
kikipatton <patton.pk16@gmail.com>
2023-03-20 09:40:50 -0700

    print lowercase in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char a = 'z';
+	for (a = 'z'; a >= 'a'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+return (0);
+}
+

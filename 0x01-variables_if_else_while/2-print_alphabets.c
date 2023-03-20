06db9ba93617f24eb260097961fc057560b605ed
kikipatton <patton.pk16@gmail.com>
2023-03-20 09:14:29 -0700

    printing alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+return (0);
+}

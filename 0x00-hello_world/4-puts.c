f64828022b393cc722b55f7d98173fd94f025919
kikipatton <patton.pk16@gmail.com>
2023-03-20 07:16:19 -0700

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this program prints a line using puts fuction
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

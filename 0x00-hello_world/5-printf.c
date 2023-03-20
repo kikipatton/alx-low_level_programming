3d09b9e50a2a48b9514c0f219931ea57b1af9487
kikipatton <patton.pk16@gmail.com>
2023-03-20 07:19:19 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This program uses printf
+ * Return: returns 0 for success
+ */
+int main(void)/*main - returns an interger*/
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}

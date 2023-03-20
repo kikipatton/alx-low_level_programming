a4efe261988f5d0bffb397bc1de03059214ac60c
kikipatton <patton.pk16@gmail.com>
2023-03-20 09:36:15 -0700

    print single digit number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int a = 0;
+	while (a < 10)
+	{
+		printf("%d", a);
+		a++;
+	}
+	printf("\n");
+return (0);
+}

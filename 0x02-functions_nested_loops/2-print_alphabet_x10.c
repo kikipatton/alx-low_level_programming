f89766ad5ff3ee40d5bdc7939da0e3d771196789
kikipatton <patton.pk16@gmail.com>
2023-03-21 10:24:13 -0700

    well nested loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*
+ * print_alphabet_x10 - repeats x10 lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int times;
+	char d;
+	for(times = 0;times <= 10;times++){
+		for (d = 'a';d <= 'z';d++){
+			_putchar(d);
+		}
+		_putchar('\n');
+	}
+}
+

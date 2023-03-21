3c7e77e1b497e4f2407d9d1fc200b2f11d9a623d
kikipatton <patton.pk16@gmail.com>
2023-03-21 10:48:57 -0700

    list lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_islower(int c) - checks for lower case character
+ */
+int _islower(int c)
+{
+	int a;
+
+	if (a <= 'z')
+	{
+		_putchar(1);
+	}
+	else {
+		_putchar(0);
+	}
+}
+

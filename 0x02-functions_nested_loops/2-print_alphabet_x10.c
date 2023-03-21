587e21bce3c8bc02e40f1634f50e2d92b38a4aa0
kikipatton <patton.pk16@gmail.com>
2023-03-21 10:34:52 -0700

    print lowercase alphabet function

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /*
- * print_alphabet_x10 - repeats x10 lowercase
+ * print_alphabet_x10 - print alphabet repeats x10 lowercase
  */
 void print_alphabet_x10(void)
 {
 	int times;
 	char d;
 
-	for(times = 1; times <= 10; times++)
+	for (times = 1; times <= 10; times++)
 	{
 		for (d = 'a'; d <= 'z'; d++)
 		{

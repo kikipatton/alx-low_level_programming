88d3e6e1d634a5eb2c5d752cae04f9ab7b189517
kikipatton <patton.pk16@gmail.com>
2023-03-21 05:55:53 -0700

    function that print alphabet in lower

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - main code here
+ * return: 0 (success)
+ */
+void print_alphabet(void);
+char print_alphabet(char){
+	char result;
+	char c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	return result;
+}
+int main(void)
+{
+	int alpha = A
+		pu
+}

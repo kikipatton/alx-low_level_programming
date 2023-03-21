6838386457ae1eb4689adc241e8707cd58ba31e4
kikipatton <patton.pk16@gmail.com>
2023-03-21 12:29:08 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_abs - compute absolute integer value
+ *@n: Varibale given to compute
+ *Return: absolute value
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}

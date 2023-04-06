7e3fa98ed202da197f999187ab973720c57f4c71
kikipatton <patton.pk16@gmail.com>
2023-04-06 13:49:48 -0700

    return natural sqroot of num

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * check -find square root
+ * @a:int
+ * @b:int
+ * Return: int
+ */
+int check(int a, int b)
+{
+	if (a * a == b)
+		return (a);
+	if (a * a > b)
+		return (-1);
+	return (check(a + 1, b));
+}
+
+/**
+ * _sqrt_recursion -returns natural sqroot of a num
+ * @n: is the integer to find sqrt of
+ * Return: natural square root or -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (check(1, n));
+}
